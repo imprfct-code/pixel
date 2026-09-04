c726d7dda992e04f182e3bd5343e48f13189e755
imprfct <68732839+imprfct@users.noreply.github.com>
2026-09-04 13:57:03 +0300

    chore: sync Convex generated API

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,14 +1,47 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as auth from "../auth.js";
 import type * as entries from "../entries.js";
 import type * as r2 from "../r2.js";
 import type * as users from "../users.js";
+
 import type { ApiFromModules, FilterApi, FunctionReference } from "convex/server";
 
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
   entries: typeof entries;
   r2: typeof r2;
   users: typeof users;
 }>;
 
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
 export declare const api: FilterApi<typeof fullApi, FunctionReference<any, "public">>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
 export declare const internal: FilterApi<typeof fullApi, FunctionReference<any, "internal">>;
-export declare const components: any;
+
+export declare const components: {
+  r2: import("@convex-dev/r2/_generated/component.js").ComponentApi<"r2">;
+};
